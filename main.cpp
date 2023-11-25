589c3d6d8414d4171d23c742609c55ad375c2674
Luisa Apfelbach <luisa.apfelbach@gmail.com>
2023-11-25 01:39:20 +0100

    ich bin müde

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
-// intelligenter Taschenrechner
+// dämlicher Taschenrechner
 #include "std_lib_inc.h"
-
-int schrift_zu_ziffer(string x)         // konvertiert das geschriebene Wort in eine Zahl
-{
-    if (x == "ein" || x == "eins")      // ermöglicht Erkennung von "eins" und "ein" als 1
+int schrift_zu_ziffer(string x) {                                                                                       // konvertiert das geschriebene Wort in eine Zahl
+    if (x == "ein" || x == "eins")                                                                                      // ermöglicht Erkennung von "eins" und "ein" als 1
         return 1;
     else if (x == "zwei")
         return 2;
@@ -25,29 +23,23 @@ int schrift_zu_ziffer(string x)         // konvertiert das geschriebene Wort in 
         return 10;
     else if (x == "null")
         return 0;
-    else {                  // abweichende Eingabe wird als ungültig erkannt
+    else {                                                                                                              // abweichende Eingabe wird als ungültig erkannt
         cout << "Ungültige Eingabe für schrift_zu_ziffer: " << x << endl;
         return -1;
     }
 }
 
 
-// Eingabe einer Rechenoperation und Berechnung d. Ergebnisses
-
-int addition(int x, int y)
-{
+int addition(int x, int y) {
     return x+y;
 }
-int subtraktion(int x, int y)
-{
+int subtraktion(int x, int y) {
     return x-y;
 }
-int multiplikation(int x, int y)
-{
+int multiplikation(int x, int y) {
     return x*y;
 }
-double division(int x, int y)
-{
+double division(int x, int y) {
     if(y != 0){
         return double(x)/y;
     }
@@ -67,9 +59,10 @@ double geteilt(int x, int y)
     }
 }
 
+
+
 // Ausgabe
-int main()
-{
+int main() {
     string operand1, operation, operand2;
     vector<double>ergebnisse={}; // vector um Ergebnisse zu speichern
 
@@ -78,45 +71,51 @@ int main()
         if(operand1 == "ende") {
             break;
         }
+            // Debugging: die Richtigkeit vom Operanden muss vorher schon überprüft werden, sonst wird die Fehlermeldung so oft ausgegeben, wie die Schleife wiederholt wird
+        else if(!(operand1 == "schrift_zu_ziffer")) {
+            cout << "Der erste Operand konnte nicht gelesen werden.\n";
+            continue;
+        }
+
+
         cin >> operation;
         cin >> operand2;
 
         int x = schrift_zu_ziffer(operand1);
         int y = schrift_zu_ziffer(operand2);
 
-        if(operation == "plus")
-        {
+        if(operation == "plus") {
             ergebnisse.push_back(addition(x, y));
-            cout << "gespeichert\n";
+            cout << "Gespeichert\n";
         }
-        else if(operation == "minus")
-        {
+        else if(operation == "minus") {
             ergebnisse.push_back(subtraktion(x, y));
-            cout << "gespeichert\n";
+            cout << "Gespeichert\n";
         }
-        else if(operation == "mal")
-        {
+        else if(operation == "mal") {
             ergebnisse.push_back(multiplikation(x, y));
-            cout << "gespeichert\n";
+            cout << "Gespeichert\n";
         }
-        else if(operation == "geteilt-durch")
-        {
-            cout << "Das Ergebnis ist: " << geteilt(x,y) << '\n';
+        else if(operation == "geteilt-durch") {
+            ergebnisse.push_back(geteilt(x, y));
+            cout << "Gespeichert\n";
         }
-        else if(operation == "dividiert-durch")
-        {
-            cout << "Das Ergbnis ist: " << division(x,y) << '\n';
+        else if(operation == "dividiert-durch") {
+            ergebnisse.push_back(division(x, y));
+            cout << "Gespeichert\n";
+
         }
-        else
-        {
-            cout << "Ungültige Operation: " << operation;
+        else {
+            cout << "Der Operator konnte nicht gelesen werden.\n";
         }
     }
     if (ergebnisse.size() == 0) {
         cout << "Es gibt keine Ergebnisse." << endl;
-    } else if (ergebnisse.size() == 1) {
+    }
+    else if (ergebnisse.size() == 1) {
         cout << "Das Ergebnis lautet: " << ergebnisse[0] << "\n";
-    } else {
+    }
+    else {
         cout << "Die Ergebnisse lauten: ";
         for (int i = 0; i < ergebnisse.size() - 1; i++) {
             cout <<  ergebnisse[i] << ", ";
